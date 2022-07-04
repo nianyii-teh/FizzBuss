728bacc73545407a228cd2955d47877037992ccb
Nian Teh <Nian.Teh@softwire.com>
2022-07-04 17:26:41 +0100

    Added comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,40 +3,54 @@ function fizzbuzz() {
     
     // Put your code here...
     for (let i = 1; i <= 100; i++) {
+        // Multiple of 11
         if (i % 11 == 0) {
 
-            // Also multiple of 13
+            // Multiple of 13?
             if (i % 13 == 0) {
                 console.log("FezzBong");
             } else {
                 console.log("Bong");
             }
+
+        // Multiple of 5
         } else if (i % 5 == 0) {
             
+            // Also multiple of 3?
             if (i % 3 == 0) {
-                if (i % 13 == 0) {
-                    console.log("FizzFezzBuzz");
-                } else {
-                    console.log("FizzBuzz");
-                }
                 
+                // Also multiple of 7?
                 if (i % 7 == 0) {
+
+                    // Also multiple of 13?
                     if (i % 13 == 0) {
                         console.log("FizzFezzBuzzBang");
                     } else {
                         console.log("FizzBuzzBang");
                     }
                 }
-            } 
-            
-            if (i % 13 == 0) {
+
+                // Also multiple of 13 but not multiple of 7?
+                if (i % 13 == 0) {
+                    console.log("FizzFezzBuzz");
+                } else {
+                    console.log("FizzBuzz");
+                }
+            // Also multiple of 13?
+            } else if (i % 13 == 0) {
                 console.log("FezzBuzz");
+            // Multiple of 5 only
             } else {
                 console.log("Buzz");
             }
 
+        // Multiple of 3?
         } else if (i % 3 == 0) {
+            
+            // Multiple of 3 and 7
             if (i % 7 == 0) {
+
+                // Multiple of 3, 7, 13?
                 if (i % 13 == 0) {
                     console.log("FizzFezzBang");
                 } else {
@@ -44,6 +58,7 @@ function fizzbuzz() {
                 }
             }
 
+            // Multiple of 3 and 
             if (i % 13 == 0) {
                 console.log("FizzFezz");
             } else {
