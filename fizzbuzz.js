29cf77c54830b18949651f055cc0397e1796c983
Nian Teh <Nian.Teh@softwire.com>
2022-07-04 17:18:25 +0100

    Added rule for multiple of 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,67 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
-
+    
     // Put your code here...
+    for (let i = 1; i <= 100; i++) {
+        if (i % 11 == 0) {
 
+            // Also multiple of 17
+            if (i % 17 == 0) {
+                console.log("FezzBong");
+            } else {
+                console.log("Bong");
+            }
+        } else if (i % 5 == 0) {
+            
+            if (i % 3 == 0) {
+                if (i % 17 == 0) {
+                    console.log("FizzFezzBuzz");
+                } else {
+                    console.log("FizzBuzz");
+                }
+                
+                if (i % 7 == 0) {
+                    if (i % 17 == 0) {
+                        console.log("FizzFezzBuzzBang");
+                    } else {
+                        console.log("FizzBuzzBang");
+                    }
+                }
+            } 
+            
+            if (i % 17 == 0) {
+                console.log("FezzBuzz");
+            } else {
+                console.log("Buzz");
+            }
+
+        } else if (i % 3 == 0) {
+            if (i % 7 == 0) {
+                if (i % 17 == 0) {
+                    console.log("FizzFezzBang");
+                } else {
+                    console.log("FizzBang");
+                }
+            }
+
+            if (i % 17 == 0) {
+                console.log("FizzFezz");
+            } else {
+                console.log("Fizz");
+            }
+
+        } else if (i % 7 == 0) {
+            if (i % 17 == 0) {
+                console.log("FezzBang");
+            } else {
+                console.log("Bang");
+            }
+        } else if (i % 17 == 0) {
+            console.log("Fezz");
+        } else {
+            console.log(i);
+        }
+    }
 }
 
 // Now, we run the main function:
