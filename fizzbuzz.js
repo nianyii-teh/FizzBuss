8e5193e6c44a0362263519bcafbb7adca329a2bd
Nian Teh <Nian.Teh@softwire.com>
2022-07-04 17:19:17 +0100

    Fixed typo between 13 and 17

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,8 +5,8 @@ function fizzbuzz() {
     for (let i = 1; i <= 100; i++) {
         if (i % 11 == 0) {
 
-            // Also multiple of 17
-            if (i % 17 == 0) {
+            // Also multiple of 13
+            if (i % 13 == 0) {
                 console.log("FezzBong");
             } else {
                 console.log("Bong");
@@ -14,14 +14,14 @@ function fizzbuzz() {
         } else if (i % 5 == 0) {
             
             if (i % 3 == 0) {
-                if (i % 17 == 0) {
+                if (i % 13 == 0) {
                     console.log("FizzFezzBuzz");
                 } else {
                     console.log("FizzBuzz");
                 }
                 
                 if (i % 7 == 0) {
-                    if (i % 17 == 0) {
+                    if (i % 13 == 0) {
                         console.log("FizzFezzBuzzBang");
                     } else {
                         console.log("FizzBuzzBang");
@@ -29,7 +29,7 @@ function fizzbuzz() {
                 }
             } 
             
-            if (i % 17 == 0) {
+            if (i % 13 == 0) {
                 console.log("FezzBuzz");
             } else {
                 console.log("Buzz");
@@ -37,26 +37,26 @@ function fizzbuzz() {
 
         } else if (i % 3 == 0) {
             if (i % 7 == 0) {
-                if (i % 17 == 0) {
+                if (i % 13 == 0) {
                     console.log("FizzFezzBang");
                 } else {
                     console.log("FizzBang");
                 }
             }
 
-            if (i % 17 == 0) {
+            if (i % 13 == 0) {
                 console.log("FizzFezz");
             } else {
                 console.log("Fizz");
             }
 
         } else if (i % 7 == 0) {
-            if (i % 17 == 0) {
+            if (i % 13 == 0) {
                 console.log("FezzBang");
             } else {
                 console.log("Bang");
             }
-        } else if (i % 17 == 0) {
+        } else if (i % 13 == 0) {
             console.log("Fezz");
         } else {
             console.log(i);
